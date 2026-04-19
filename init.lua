2c4229624a92d785e691ad7cbb1241c493c9e5a8
BabarKhanDev <b.khan01@hotmail.co.uk>
2026-04-19 13:42:58 +0100

    Enable clangd and pyright

--- init.lua
+++ init.lua
@@ -498,7 +498,6 @@ require('lazy').setup({
       vim.g.loaded_newtrwPlugin = 1
     end,
   },
-
   -- LSP Plugins
   {
     -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
@@ -708,9 +707,10 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
+        clangd = {},
         -- gopls = {},
-        -- pyright = {},
+        pyright = {},
+
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
