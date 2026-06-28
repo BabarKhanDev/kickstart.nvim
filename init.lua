d25a1b58182dfb9084c5dcfea464223e0b2a74d7
BabarKhanDev <b.khan01@hotmail.co.uk>
2026-06-28 14:00:33 +0100

    Add render-markdown

--- init.lua
+++ init.lua
@@ -269,6 +269,18 @@ require('lazy').setup({
     },
   },
   {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' },        -- if you use standalone mini plugins
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    ft = { 'markdown' },
+    opts = {
+      latex = { enabled = true, inline = true, block = true },
+    },
+  },
+  {
     'lervag/vimtex',
     lazy = false, -- we don't want to lazy load VimTeX
     -- tag = "v2.15", -- uncomment to pin to a specific release
@@ -868,6 +880,9 @@ require('lazy').setup({
         'query',
         'vim',
         'vimdoc',
+        'latex',
+        'html',
+        'yaml',
       }
 
       ts.install(ensure_installed)
